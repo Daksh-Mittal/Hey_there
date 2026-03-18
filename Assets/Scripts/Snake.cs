7af4ded47e378b1213cfb46bfb46f7f22699bd7e
Daksh Mittal <167610763+Daksh-Mittal@users.noreply.github.com>
2026-03-18 20:53:10 +0530

    Update Snake.cs

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,54 +4,55 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    // Obstacle avoidance parameters (see the assignment spec for an explanation).
+    private float _sleepTimer = 0.0f;
+
     public AvoidanceParams AvoidParams;
 
-    // Steering parameters.
     public float MaxSpeed;
     public float MaxAccel;
     public float AccelTime;
 
-    // Use this as the arrival radius for all states where the steering behaviour == arrive.
     public float ArriveRadius;
 
-    // Parameters controlling transitions in/out of the Aggro state.
     public float AggroRange;
     public float DeAggroRange;
 
-    // Reference to the frog (the target for the Aggro state).
     public GameObject Frog;
-
-    // The patrol point (the target for the PatrolAway state).
     public Transform PatrolPoint;
 
-    // The snake's initial position (the target for the PatrolHome and Harmless states).
     private Vector2 _home;
 
-    // Debug rendering config
     private float _debugHomeOffset = 0.3f;
 
-    // References for gameobject controls
     private Rigidbody2D _rb;
     private SpriteRenderer _sr;
     private Animator _animator;
 
-    // Current Snake FSM State
+    private Vector2 _fleeTarget;
+
     public SnakeState State;
 
-    // Snake FSM states (to be completed by you)
     public enum SnakeState : int
     {
-        TODO = 0
+        PatrolAway = 0,
+        PatrolHome = 1,
+        Attack = 2,
+        Harmless = 3,
+        Snooze = 4,
+        Fleeing = 5   // New
     }
 
-    // Snake FSM events (to be completed by you)
     public enum SnakeEvent : int
     {
-        TODO = 0
+        FrogInRange = 0,
+        FrogOutOfRange = 1,
+        BitFrog = 2,
+        ReachedTarget = 3,
+        TimerOff = 4,
+        HitByBubble = 5,   
+        NotScared = 6      
     }
 
-    // Direction IDs used by the snake animator (please don't edit these).
     private enum Direction : int
     {
         Up = 0,
@@ -69,37 +70,93 @@ public class Snake : MonoBehaviour
         _home = transform.position;
     }
 
-    // Our common FSM approach has been setup for you.
-    // This is an event-first FSM, where events can be triggered by FixedUpdateEvents().
-    // Then FSM_State() processes the current FSM state.
-    // UpdateAppearance() is called at the end to update the snake's appearance.
     void FixedUpdate()
     {
-        // Events triggered by each fixed update tick
         FixedUpdateEvents();
-
-        // Update the Snake behaviour based on the current FSM state
         FSM_State();
-
-        // Configure final appearance of the snake
         UpdateAppearance();
-    } 
-
-    // Trigger Events for each fixed update tick, using a trigger first FSM implementation
-    void FixedUpdateEvents()
-    {
-        
     }
 
+    void FixedUpdateEvents()
+    {
+        if (State == SnakeState.Snooze)
+        {
+            _sleepTimer += Time.fixedDeltaTime;
 
-    // Process the current FSM state, using an event first FSM implementation
-    // This currently has a zero steering force.
-    // You need to implement the steering logic depending on the FSM state.
+            if (_sleepTimer >= Constants.SLEEP_TIME)
+            {
+                HandleEvent(SnakeEvent.TimerOff);
+                _sleepTimer = 0.0f;
+            }
+        }
+
+        if (State == SnakeState.Fleeing)
+        {
+            if (Vector2.Distance(transform.position, _fleeTarget) <= Constants.TARGET_REACHED_TOLERANCE)
+            {
+                HandleEvent(SnakeEvent.NotScared);
+            }
+        }
+
+        if (Frog != null && State != SnakeState.Fleeing)
+        {
+            float distToFrog = Vector2.Distance(transform.position, Frog.transform.position);
+
+            if (distToFrog <= AggroRange)
+                HandleEvent(SnakeEvent.FrogInRange);
+
+            if (distToFrog > DeAggroRange)
+                HandleEvent(SnakeEvent.FrogOutOfRange);
+        }
+
+        Vector2 target = transform.position;
+
+        if (State == SnakeState.PatrolAway)
+            target = PatrolPoint.position;
+
+        else if (State == SnakeState.PatrolHome || State == SnakeState.Harmless)
+            target = _home;
+
+        if (State == SnakeState.PatrolAway ||
+            State == SnakeState.PatrolHome ||
+            State == SnakeState.Harmless)
+        {
+            if (Vector2.Distance(transform.position, target) <= Constants.TARGET_REACHED_TOLERANCE)
+            {
+                HandleEvent(SnakeEvent.ReachedTarget);
+            }
+        }
+    }
+
     void FSM_State()
     {
         Vector2 desiredVel = Vector2.zero;
 
-        // Convert the desired velocity to a force, then apply it.
+        if (State == SnakeState.PatrolAway)
+        {
+            desiredVel = Steering.Arrive(transform.position, PatrolPoint.position, ArriveRadius, MaxSpeed, AvoidParams);
+        }
+        else if (State == SnakeState.PatrolHome)
+        {
+            desiredVel = Steering.Arrive(transform.position, _home, ArriveRadius, MaxSpeed, AvoidParams);
+        }
+        else if (State == SnakeState.Attack)
+        {
+            desiredVel = Steering.Seek(transform.position, Frog.transform.position, MaxSpeed, AvoidParams);
+        }
+        else if (State == SnakeState.Harmless)
+        {
+            desiredVel = Steering.Arrive(transform.position, _home, ArriveRadius, MaxSpeed, AvoidParams);
+        }
+        else if (State == SnakeState.Snooze)
+        {
+            desiredVel = Vector2.zero;
+        }
+        else if (State == SnakeState.Fleeing)
+        {
+            desiredVel = Steering.SeekDirect(transform.position, _fleeTarget, MaxSpeed); // move to flee target
+        }
+
         Vector2 steering = Steering.DesiredVelToForce(desiredVel, _rb, AccelTime, MaxAccel);
         _rb.AddForce(steering);
     }
@@ -108,8 +165,11 @@ public class Snake : MonoBehaviour
     {
         if (newState != State)
         {
-            // Can uncomment this for debugging purposes.
-            //Debug.Log(name + " switching state to " + newState.ToString());
+            if (newState == SnakeState.Fleeing && Frog != null)
+            {
+                Vector2 dir = (transform.position - Frog.transform.position).normalized;
+                _fleeTarget = (Vector2)transform.position + dir * 5f; // distance tweakable
+            }
 
             State = newState;
         }
@@ -117,43 +177,117 @@ public class Snake : MonoBehaviour
 
     private void HandleEvent(SnakeEvent e)
     {
-    }
-    private void UpdateAppearance()
-    {
-        // Update the snake's colour to provide a visual indication of its state.
-        // This is for you to implement
-
-        // Update the Snake visual based on the direction it's moving
-        // (please don't modify this block)
-        if (_rb.linearVelocity.magnitude > Constants.MIN_SPEED_TO_ANIMATE)
+        if (e == SnakeEvent.HitByBubble && State != SnakeState.Fleeing)
         {
-            // Determine the bearing of the snake in degrees (between -180 and 180)
-            float angle = Mathf.Atan2(_rb.linearVelocity.y, _rb.linearVelocity.x) * Mathf.Rad2Deg;
-
-            if (angle > -135.0f && angle <= -45.0f) // Down
-            {
-                transform.up = new Vector2(0.0f, -1.0f);
-                _animator.SetInteger("Direction", (int)Direction.Down);
-            }
-            else if (angle > -45.0f && angle <= 45.0f) // Right
-            {
-                transform.up = new Vector2(1.0f, 0.0f);
-                _animator.SetInteger("Direction", (int)Direction.Right);
-            }
-            else if (angle > 45.0f && angle <= 135.0f) // Up
-            {
-                transform.up = new Vector2(0.0f, 1.0f);
-                _animator.SetInteger("Direction", (int)Direction.Up);
-            }
-            else // Left
-            {
-                transform.up = new Vector2(-1.0f, 0.0f);
-                _animator.SetInteger("Direction", (int)Direction.Left);
-            }
+            SetState(SnakeState.Fleeing);
+            return;
         }
 
-        // Display the Snake home position as a cross
-        Debug.DrawLine(_home + new Vector2(-_debugHomeOffset, -_debugHomeOffset), _home + new Vector2(_debugHomeOffset, _debugHomeOffset), Color.magenta);
-        Debug.DrawLine(_home + new Vector2(-_debugHomeOffset, _debugHomeOffset), _home + new Vector2(_debugHomeOffset, -_debugHomeOffset), Color.magenta);
+        if (State == SnakeState.Fleeing)
+        {
+            if (e == SnakeEvent.NotScared)
+                SetState(SnakeState.PatrolHome);
+
+            return;
+        }
+
+        if (State == SnakeState.PatrolAway)
+        {
+            if (e == SnakeEvent.ReachedTarget)
+                SetState(SnakeState.PatrolHome);
+
+            else if (e == SnakeEvent.FrogInRange)
+                SetState(SnakeState.Attack);
+        }
+
+        else if (State == SnakeState.PatrolHome)
+        {
+            if (e == SnakeEvent.ReachedTarget)
+                SetState(SnakeState.PatrolAway);
+
+            else if (e == SnakeEvent.FrogInRange)
+                SetState(SnakeState.Attack);
+        }
+
+        else if (State == SnakeState.Attack)
+        {
+            if (e == SnakeEvent.FrogOutOfRange)
+                SetState(SnakeState.PatrolHome);
+
+            else if (e == SnakeEvent.BitFrog)
+                SetState(SnakeState.Snooze);
+        }
+
+        else if (State == SnakeState.Snooze)
+        {
+            if (e == SnakeEvent.TimerOff)
+                SetState(SnakeState.Harmless);
+        }
+
+        else if (State == SnakeState.Harmless)
+        {
+            if (e == SnakeEvent.ReachedTarget)
+                SetState(SnakeState.PatrolHome);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Frog"))
+        {
+            HandleEvent(SnakeEvent.BitFrog);
+        }
+
+        if (collider.CompareTag("Bubble"))
+        {
+            HandleEvent(SnakeEvent.HitByBubble);
+        }
+    }
+
+    private void UpdateAppearance()
+    {
+        if (State == SnakeState.PatrolAway)
+            _sr.color = new Color(0.5f, 0.5f, 0.5f);
+
+        else if (State == SnakeState.PatrolHome)
+            _sr.color = new Color(1f, 1f, 1f);
+
+        else if (State == SnakeState.Attack)
+            _sr.color = new Color(1f, 0.1f, 0.1f);
+
+        else if (State == SnakeState.Harmless)
+            _sr.color = new Color(0.2f, 0.9f, 0.2f);
+
+        else if (State == SnakeState.Snooze)
+            _sr.color = new Color(0.2f, 0.2f, 0.9f);
+
+        else if (State == SnakeState.Fleeing)
+            _sr.color = new Color(0.9f, 0.7f, 0.2f); 
+
+        if (_rb.linearVelocity.magnitude > Constants.MIN_SPEED_TO_ANIMATE)
+        {
+            float angle = Mathf.Atan2(_rb.linearVelocity.y, _rb.linearVelocity.x) * Mathf.Rad2Deg;
+
+            if (angle > -135 && angle <= -45)
+            {
+                transform.up = Vector2.down;
+                _animator.SetInteger("Direction", 2);
+            }
+            else if (angle > -45 && angle <= 45)
+            {
+                transform.up = Vector2.right;
+                _animator.SetInteger("Direction", 3);
+            }
+            else if (angle > 45 && angle <= 135)
+            {
+                transform.up = Vector2.up;
+                _animator.SetInteger("Direction", 0);
+            }
+            else
+            {
+                transform.up = Vector2.left;
+                _animator.SetInteger("Direction", 1);
+            }
+        }
     }
 }
